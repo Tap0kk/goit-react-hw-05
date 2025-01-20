524e49120f7619e0bbf0532e50b3dcc513f87f1c
Deman <deman2805@gmail.com>
2025-01-20 13:53:40 +0200

    fix errror

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,8 @@
 import s from './Error.module.css';
-import ooopsImg from '../../images/ooops.jpg';
 
 const Error = ({ err }) => {
   return (
     <div className={s.wrap}>
-      <img className={s.img} src={ooopsImg} alt="Funny face" />
-
       <div className={s.message_wrap}>
         <p className={s.title}>Oooops, something get wrong...</p>
 
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,9 @@
 import s from './Section.module.css';
 
-const Section = ({ children, title, titleError, isError }) => {
+const Section = ({ children, title, isError }) => {
   return (
     <section className={s.section}>
-      {isError ? (
-        <h2 className={s.titleError}>{titleError}</h2>
-      ) : (
-        <h2 className={s.title}>{title}</h2>
-      )}
+      {isError ? '' : <h2 className={s.title}>{title}</h2>}
       {children}
     </section>
   );
